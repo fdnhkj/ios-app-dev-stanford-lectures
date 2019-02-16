caba39a08ba708112089a4c057a3563ac9daa944
Fidan Hakaj <fidan_188@hotmail.com>
2019-02-17 00:27:01 +0100

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Fidan Hakaj on 17/02/2019.
+//  Copyright © 2019 Fidan Hakaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
